ee6797e9396193c4c9f412b632387fe417ace877
Chris-Bortel <bortelcc19@gmail.com>
2020-04-25 12:51:13 -0700

    constructor function has been completed

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
 'use strict';
+var productArray = [];
 
-console.log('I am working');
+function Product(name, src) {
+  this.name = name;
+  this.src = src;
+  this.click = 0;
+
+  productArray.push(this);
+}
+
+new Product('bag', 'assets/bag.jpg');
+new Product('banana', 'assets/banana.jpg');
+new Product('bathroom', 'assets/bathroom.jpg');
+new Product('boots', 'assets/boots.jpg');
+// new Product('breakfast', 'assets/breakfast.jpg');
+// new Product('bubblegum', 'assets/bubblegum.jpg');
+// new Product('chair', 'assets/chair.jpg');
+// new Product('cthulhu', 'assets/cthulhu.jpg');
+// new Product('dog-duck', 'assets/dog-duck.jpg');
+// new Product('scissors', 'assets/scissors.jpg');
+// new Product('shark', 'assets/shark.jpg');
+// new Product('pet-sweep', 'assets/sweep.jpg');
+// new Product('tauntaun', 'assets/tauntaun.jpg');
+// new Product('unicorn', 'assets/unicorn.jpg');
+// new Product('unicorn', 'assets/unicorn.jpg');
+// // new Product('usb', 'assets/usb.jpg');
+// new Product('water-can', 'assets/water-can.jpg');
+// new Product('wine-glass', 'assets/wine-glass.jpg');
 
