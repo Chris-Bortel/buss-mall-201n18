f5f362d87eea2c98ae65b9e7e702363f252bbf60
Chris-Bortel <bortelcc19@gmail.com>
2020-04-27 16:12:13 -0700

    percentages are switched to vw, vh, and rem. It is almost responsive.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var productThreeEl = document.getElementById('product-3');
 var divEl = document.getElementById('product-container');
 var productList = document.getElementById('productList');
 
-var clickTracker = 3;
+var clickTracker = 4;
 
 function Product(name, src) {
   this.name = name;
@@ -51,8 +51,8 @@ function stopClicking() {
 new Product('bag', 'assets/bag.jpg');
 new Product('banana', 'assets/banana.jpg');
 new Product('bathroom', 'assets/bathroom.jpg');
-new Product('boots', 'assets/boots.jpg');
-new Product('breakfast', 'assets/breakfast.jpg');
+// new Product('boots', 'assets/boots.jpg');
+// new Product('breakfast', 'assets/breakfast.jpg');
 // new Product('bubblegum', 'assets/bubblegum.jpg');
 // new Product('chair', 'assets/chair.jpg');
 // new Product('cthulhu', 'assets/cthulhu.jpg');
@@ -63,7 +63,7 @@ new Product('breakfast', 'assets/breakfast.jpg');
 // new Product('tauntaun', 'assets/tauntaun.jpg');
 // new Product('unicorn', 'assets/unicorn.jpg');
 // new Product('unicorn', 'assets/unicorn.jpg');
-// // new Product('usb', 'assets/usb.jpg');
+// new Product('usb', 'assets/usb.gif');
 // new Product('water-can', 'assets/water-can.jpg');
 // new Product('wine-glass', 'assets/wine-glass.jpg');
 Product.prototype.renderProductList = function () {
