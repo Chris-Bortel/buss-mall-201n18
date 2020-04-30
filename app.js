dd4a9e95889c4419b8d1e152915024ce0ace75a7
Chris-Bortel <bortelcc19@gmail.com>
2020-04-29 19:11:08 -0700

    object naming conventions are cleaned up. Next step: update randomizer algorithm so that there are no duplicate products being displays both on the current rendering and the previous.

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ var productThreeEl = document.getElementById('product-3');
 var divEl = document.getElementById('product-container');
 var productList = document.getElementById('productList');
 
-var clickTracker = 25;
+var clickTracker = 4;
 
 function Product(title, src) {
-  // add clicked = 0 and views = 0
+  // TODO: add clicked = 0 and views = 0
   this.productTitle = title;
-  this.src = src;
-  this.alt = title;
+  this.productSrc = src;
+  this.productAlt = title;
   this.views = 0;
   this.clicked = 0;
 
@@ -23,14 +23,11 @@ function Product(title, src) {
 
 //randomizer
 function randomizer (max) {
-  return Math.floor(Math.random() * max); 
+  return Math.floor(Math.random() * max);
 }
 
 function imageGenerator() {
 //TODO: in order to make this have no repeats, use a do while
-  // do {
-
-  //
   do {
     var product1 = randomizer(productArray.length);
     var product2 = randomizer(productArray.length);
@@ -39,18 +36,18 @@ function imageGenerator() {
   ((product1 === product2) || (product2 === product1) || (product3 === product2) || (product3 === product1));
 
   productOneEl.title = productArray[product1].productTitle;
-  productOneEl.src = productArray[product1].src;
-  productOneEl.alt = productArray[product1].alt;
+  productOneEl.src = productArray[product1].productSrc;
+  productOneEl.alt = productArray[product1].productAlt;
   productArray[product1].views++;
 
   productTwoEl.title = productArray[product2].productTitle;
-  productTwoEl.src = productArray[product2].src;
-  productTwoEl.alt = productArray[product2].alt;
+  productTwoEl.src = productArray[product2].productSrc;
+  productTwoEl.alt = productArray[product2].productAlt;
   productArray[product2].views++;
 
   productThreeEl.title = productArray[product3].productTitle;
-  productThreeEl.src = productArray[product3].src;
-  productThreeEl.alt = productArray[product3].alt;
+  productThreeEl.src = productArray[product3].productSrc;
+  productThreeEl.alt = productArray[product3].productalt;
   productArray[product3].views++;
   // imageGenerator();
 }
@@ -64,20 +61,20 @@ new Product('bag', 'assets/bag.jpg');
 new Product('banana', 'assets/banana.jpg');
 new Product('bathroom', 'assets/bathroom.jpg');
 new Product('boots', 'assets/boots.jpg');
-new Product('breakfast', 'assets/breakfast.jpg');
-new Product('bubblegum', 'assets/bubblegum.jpg');
-new Product('chair', 'assets/chair.jpg');
-new Product('cthulhu', 'assets/cthulhu.jpg');
-new Product('dog-duck', 'assets/dog-duck.jpg');
-new Product('scissors', 'assets/scissors.jpg');
-new Product('shark', 'assets/shark.jpg');
-new Product('pet-sweep', 'assets/sweep.jpg');
-new Product('tauntaun', 'assets/tauntaun.jpg');
-new Product('unicorn', 'assets/unicorn.jpg');
+// new Product('breakfast', 'assets/breakfast.jpg');
+// new Product('bubblegum', 'assets/bubblegum.jpg');
+// new Product('chair', 'assets/chair.jpg');
+// new Product('cthulhu', 'assets/cthulhu.jpg');
+// new Product('dog-duck', 'assets/dog-duck.jpg');
+// new Product('scissors', 'assets/scissors.jpg');
+// new Product('shark', 'assets/shark.jpg');
+// new Product('pet-sweep', 'assets/sweep.jpg');
+// new Product('tauntaun', 'assets/tauntaun.jpg');
 // new Product('unicorn', 'assets/unicorn.jpg');
-new Product('usb', 'assets/usb.gif');
-new Product('water-can', 'assets/water-can.jpg');
-new Product('wine-glass', 'assets/wine-glass.jpg');
+// // new Product('unicorn', 'assets/unicorn.jpg');
+// new Product('usb', 'assets/usb.gif');
+// new Product('water-can', 'assets/water-can.jpg');
+// new Product('wine-glass', 'assets/wine-glass.jpg');
 Product.prototype.renderProductList = function () {
   var productListUlElement = document.createElement('li');
   // productListUlElement.textContent = 'ergasdfg';
@@ -105,15 +102,13 @@ function handleClick(event){
     // need to add a function to track the number of clicks that the user has made.
   }
   // console.log(productArray);
-  //each time product is clicked the available click number goes down one
   clickTracker--;
 
   if(clickTracker === 0) {
     for(i = 0 ; i < productArray.length; i++) {
       productArray[i].renderProductList();
     }
-    // Product.prototype.renderProductList();
-    // Product.prototype.renderProductListClicks();
+
 
     stopClicking();
 
