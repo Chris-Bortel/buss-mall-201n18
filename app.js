4ea832ac4663874cf519097f7c53ad374cbceb03
Chris-Bortel <bortelcc19@gmail.com>
2020-04-30 20:59:09 -0700

    fall back

--- app.js
+++ app.js
@@ -1,85 +1,59 @@
 'use strict';
 var productArray = [];
 
-
-// var productOneEl = document.getElementById('product-1');
-// var productTwoEl = document.getElementById('product-2');
-// var productThreeEl = document.getElementById('product-3');
+var productOneEl = document.getElementById('product-1');
+var productTwoEl = document.getElementById('product-2');
+var productThreeEl = document.getElementById('product-3');
 var divEl = document.getElementById('product-container');
 var productList = document.getElementById('productList');
 
-var clickTracker = 10;
-var uniqueSixArray = [];
-var unique = 6;
-// notes
+var clickTracker = 25;
+
 function Product(title, src) {
-  // TODO: add clicked = 0 and views = 0
+  // add clicked = 0 and views = 0
   this.productTitle = title;
-  this.productSrc = src;
-  this.productAlt = title;
+  this.src = src;
+  this.alt = title;
   this.views = 0;
   this.clicked = 0;
 
   productArray.push(this);
   // console.log(this.click);
 }
-console.log(uniqueSixArray);
 
 //randomizer
-function randomizer () {
-  return Math.floor(Math.random() * productArray.length);
+function randomizer (max) {
+  return Math.floor(Math.random() * max); 
 }
-//make six unique numbers out of an array
-function uniqueSix () {
-  
-  for( var i = 0 ; i < unique ; i++) {
-    var oneProductOfSix = randomizer();
-    //first create the six
-    //then do the pop
-    uniqueSixArray.push(Math.floor(oneProductOfSix));
-  }
+
+function imageGenerator() {
+//TODO: in order to make this have no repeats, use a do while
   // do {
-   
-  // } while (oneProductOfSix === uniqueSixArray[i]);
+
+  //
+  do {
+    var product1 = randomizer(productArray.length);
+    var product2 = randomizer(productArray.length);
+    var product3 = randomizer(productArray.length);
+  } while
+  ((product1 === product2) || (product2 === product1) || (product3 === product2) || (product3 === product1));
+
+  productOneEl.title = productArray[product1].productTitle;
+  productOneEl.src = productArray[product1].src;
+  productOneEl.alt = productArray[product1].alt;
+  productArray[product1].views++;
+
+  productTwoEl.title = productArray[product2].productTitle;
+  productTwoEl.src = productArray[product2].src;
+  productTwoEl.alt = productArray[product2].alt;
+  productArray[product2].views++;
+
+  productThreeEl.title = productArray[product3].productTitle;
+  productThreeEl.src = productArray[product3].src;
+  productThreeEl.alt = productArray[product3].alt;
+  productArray[product3].views++;
+  // imageGenerator();
 }
-
-// use pop method to pop the last three off the back and assign those to prod1 prod2 prod3 variables
-//// these three are rendered to the screen.
-// then, randomizer pushes three unique numbers to the front of uniqueSixArray
-
-//come up with way to keep this populated
-//// a while loop inside of a while loop
-
-// use push method to add the remaining three to the
-//  write a function to compare three old ones to three new ones
-//
-
-
-// function imageGenerator() {
-// //TODO: in order to make this have no repeats, use a do while
-//   do {
-// var product1 = this.randomizer(productArray.length);
-// var product2 = this.randomizer(productArray.length);
-// var product3 = this.randomizer(productArray.length);
-//   } while
-//   ((product1 === product2) || (product2 === product1) || (product3 === product2) || (product3 === product1));
-
-//   productOneEl.title = productArray[product1].productTitle;
-//   productOneEl.src = productArray[product1].productSrc;
-//   productOneEl.alt = productArray[product1].productAlt;
-//   productArray[product1].views++;
-
-//   productTwoEl.title = productArray[product2].productTitle;
-//   productTwoEl.src = productArray[product2].productSrc;
-//   productTwoEl.alt = productArray[product2].productAlt;
-//   productArray[product2].views++;
-
-//   productThreeEl.title = productArray[product3].productTitle;
-//   productThreeEl.src = productArray[product3].productSrc;
-//   productThreeEl.alt = productArray[product3].productalt;
-//   productArray[product3].views++;
-//   // imageGenerator();
-// }
 
 function stopClicking() {
   divEl.removeEventListener('click', handleClick);
@@ -94,15 +68,16 @@ new Product('breakfast', 'assets/breakfast.jpg');
 new Product('bubblegum', 'assets/bubblegum.jpg');
 new Product('chair', 'assets/chair.jpg');
 new Product('cthulhu', 'assets/cthulhu.jpg');
-// new Product('dog-duck', 'assets/dog-duck.jpg');
-// new Product('scissors', 'assets/scissors.jpg');
-// new Product('shark', 'assets/shark.jpg');
-// new Product('pet-sweep', 'assets/sweep.jpg');
-// new Product('tauntaun', 'assets/tauntaun.jpg');
+new Product('dog-duck', 'assets/dog-duck.jpg');
+new Product('scissors', 'assets/scissors.jpg');
+new Product('shark', 'assets/shark.jpg');
+new Product('pet-sweep', 'assets/sweep.jpg');
+new Product('tauntaun', 'assets/tauntaun.jpg');
+new Product('unicorn', 'assets/unicorn.jpg');
 // new Product('unicorn', 'assets/unicorn.jpg');
-// new Product('usb', 'assets/usb.gif');
-// new Product('water-can', 'assets/water-can.jpg');
-// new Product('wine-glass', 'assets/wine-glass.jpg');
+new Product('usb', 'assets/usb.gif');
+new Product('water-can', 'assets/water-can.jpg');
+new Product('wine-glass', 'assets/wine-glass.jpg');
 Product.prototype.renderProductList = function () {
   var productListUlElement = document.createElement('li');
   // productListUlElement.textContent = 'ergasdfg';
@@ -130,20 +105,22 @@ function handleClick(event){
     // need to add a function to track the number of clicks that the user has made.
   }
   // console.log(productArray);
+  //each time product is clicked the available click number goes down one
   clickTracker--;
 
   if(clickTracker === 0) {
     for(i = 0 ; i < productArray.length; i++) {
       productArray[i].renderProductList();
     }
-
+    // Product.prototype.renderProductList();
+    // Product.prototype.renderProductListClicks();
 
     stopClicking();
 
   }
-  // imageGenerator();
+  imageGenerator();
 }
 
 divEl.addEventListener('click', handleClick);
 
-// imageGenerator();
+imageGenerator();
