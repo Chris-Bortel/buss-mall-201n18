f42e533abe5936470f36248c97e1c6333221ed14
Chris-Bortel <bortelcc19@gmail.com>
2020-05-07 17:04:09 -0700

    chart is now rendering with proper data, but it is showing each time that an item is clicked

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var randomArray = [];
 
 var clickTracker = 10;
 
-function Product( src, title, clicked = 0, views = 0) {
+function Product(src, title, clicked = 0, views = 0) {
   this.src = src;
   this.title = title;
   this.alt = title;
@@ -36,18 +36,18 @@ function fillArray() {
       randomArray.push(tempRandomNumber);
     }
   }
-  console.log(randomArray)
+  console.log(randomArray);
 }
 
 function imageGenerator() {
   fillArray();
   // console.log("i am filled", fillArray)
   var product1 = randomArray.shift();
-  var product2 = randomArray.shift();;
-  var product3 = randomArray.shift();;
-console.log('product1: ', product1)
-console.log('product2: ', product2)
-console.log('product3: ', product3)
+  var product2 = randomArray.shift();
+  var product3 = randomArray.shift();
+  console.log("product1: ", product1);
+  console.log("product2: ", product2);
+  console.log("product3: ", product3);
   productOneEl.src = productArray[product1].src;
   // console.log(productOneEl.src);
   // console.log(productArray[product1].src);
@@ -81,10 +81,8 @@ function saveLocalStorage() {
 // console.log(imageGenerator)
 
 function loadLocalStorage() {
-
   if (localStorage.getItem("storedproducts")) {
-    var getproducts = JSON.parse(
-      localStorage.getItem("storedproducts"));
+    var getproducts = JSON.parse(localStorage.getItem("storedproducts"));
     console.log(getproducts);
     for (var i = 0; i < getproducts.length; i++) {
       new Product(
@@ -92,121 +90,118 @@ function loadLocalStorage() {
         getproducts[i].title,
         getproducts[i].clicked,
         getproducts[i].views
-        );
-        console.log(getproducts.length)
-        // console.log(loadLocalStorage());
+      );
+      console.log(getproducts.length);
+      // console.log(loadLocalStorage());
     }
   } else {
     new Product("assets/bag.jpg", "bag");
-    new Product( "assets/banana.jpg", "banana");
-    new Product( "assets/bathroom.jpg", "bathroom");
-    new Product( "assets/boots.jpg", "boots");
-    new Product( "assets/breakfast.jpg", "breakfast");
-    new Product( "assets/bubblegum.jpg", "bubblegum");
-    new Product( "assets/chair.jpg", "chair");
-    new Product( "assets/cthulhu.jpg", "cthulhu");
-    new Product( "assets/dog-duck.jpg", "dog-duck");
-    new Product( 'assets/scissors.jpg', 'scissors');
-    new Product( 'assets/shark.jpg', 'shark');
-    new Product( 'assets/sweep.jpg', 'pet-sweep');
-    new Product( 'assets/tauntaun.jpg', 'tauntaun');
-    new Product( 'assets/unicorn.jpg', 'unicorn');
+    new Product("assets/banana.jpg", "banana");
+    new Product("assets/bathroom.jpg", "bathroom");
+    new Product("assets/boots.jpg", "boots");
+    new Product("assets/breakfast.jpg", "breakfast");
+    new Product("assets/bubblegum.jpg", "bubblegum");
+    new Product("assets/chair.jpg", "chair");
+    new Product("assets/cthulhu.jpg", "cthulhu");
+    new Product("assets/dog-duck.jpg", "dog-duck");
+    new Product("assets/scissors.jpg", "scissors");
+    new Product("assets/shark.jpg", "shark");
+    new Product("assets/sweep.jpg", "pet-sweep");
+    new Product("assets/tauntaun.jpg", "tauntaun");
+    new Product("assets/unicorn.jpg", "unicorn");
 
-    new Product( 'assets/usb.gif', 'usb');
-    new Product( 'assets/water-can.jpg', 'water-can');
-    new Product( 'assets/wine-glass.jpg', 'wine-glass');
-      
+    new Product("assets/usb.gif", "usb");
+    new Product("assets/water-can.jpg", "water-can");
+    new Product("assets/wine-glass.jpg", "wine-glass");
 
-  imageGenerator();
+    imageGenerator();
   }
 }
 
-
-// function seedChartData() {
-//   var labelArray = [];
-//   var clickedArray = [];
-//   var viewedArray = [];
-//   console.log(viewedArray);
-//   for (var i = 0; i < productArray.length; i++) {
-//     labelArray.push(productArray[i].title);
-//     clickedArray.push(productArray[i].clicked);
-//     viewedArray.push(productArray[i].views);
-//   }
-//   return [labelArray, clickedArray,  viewedArray];
-// }
-
+function seedChartData() {
+  var labelArray = [];
+  var clickedArray = [];
+  var viewedArray = [];
+  console.log(viewedArray);
+  for (var i = 0; i < productArray.length; i++) {
+    clickedArray.push(productArray[i].clicked);
+    labelArray.push(productArray[i].title);
+    viewedArray.push(productArray[i].views);
+  }
+  return [clickedArray, labelArray,  viewedArray];
+}
 
 // console.log(stopClicking())
 
-//TODO: build a function that generates colors for each item on the chart
-// function renderChart() {
-//   var ctx = document.getElementById("myChart");
-//   var myChart = new Chart(ctx, {
-//     type: "bar",
-//     data: {
-//       labels: seedChartData()[1],
-//       datasets: [
-//         {
-//           label: "# of Votes",
-//           data: seedChartData()[0],
-//           backgroundColor: [
-//             "rgba(255, 99, 132, 0.2)",
-//             "rgba(54, 162, 235, 0.2)",
-//             "rgba(255, 206, 86, 0.2)",
-//             "rgba(75, 192, 192, 0.2)",
-//             "rgba(153, 102, 255, 0.2)",
-//             "rgba(255, 159, 64, 0.2)",
-//           ],
-//           borderColor: [
-//             "rgba(255, 99, 132, 1)",
-//             "rgba(54, 162, 235, 1)",
-//             "rgba(255, 206, 86, 1)",
-//             "rgba(75, 192, 192, 1)",
-//             "rgba(153, 102, 255, 1)",
-//             "rgba(255, 159, 64, 1)",
-//           ],
-//           borderWidth: 1,
-//         },
-//         {
-//           label: "# of Views",
-//           data: seedChartData()[2],
-//           backgroundColor: [
-//             "rgba(255, 99, 132, 0.2)",
-//             "rgba(54, 162, 235, 0.2)",
-//             "rgba(255, 206, 86, 0.2)",
-//             "rgba(75, 192, 192, 0.2)",
-//             "rgba(153, 102, 255, 0.2)",
-//             "rgba(255, 159, 64, 0.2)",
-//           ],
-//           borderColor: [
-//             "rgba(255, 99, 132, 1)",
-//             "rgba(54, 162, 235, 1)",
-//             "rgba(255, 206, 86, 1)",
-//             "rgba(75, 192, 192, 1)",
-//             "rgba(153, 102, 255, 1)",
-//             "rgba(255, 159, 64, 1)",
-//           ],
-//           borderWidth: 1,
-//         },
-//       ],
-//     },
-//     options: {
-//       scales: {
-//         yAxes: [
-//           {
-//             ticks: {
-//               beginAtZero: true,
-//             },
-//           },
-//         ],
-//       },
-//     },
-//   });
-// }
+// TODO: build a function that generates colors for each item on the chart
+function renderChart() {
+  var ctx = document.getElementById("myChart");
+  var myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: seedChartData()[1],
+      datasets: [
+        {
+          label: "# of Votes",
+          data: seedChartData()[0],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+        },
+        {
+          label: "# of Views",
+          data: seedChartData()[2],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+    },
+  });
+}
 
 Product.prototype.renderProductList = function () {
   var productListUlElement = document.createElement("li");
-console.log (productListUlElement);
+  console.log(productListUlElement);
   productListUlElement.textContent =
     this.title + ": " + this.clicked + ", viewed " + this.views;
   productList.appendChild(productListUlElement);
@@ -220,7 +215,7 @@ Product.prototype.renderProductListClicks = function () {
 
 function handleClick(event) {
   // console.log(event);
-  
+
   var clickedProductImage = event.target.title;
   clickTracker--;
   console.log(clickedProductImage);
@@ -232,38 +227,44 @@ function handleClick(event) {
   }
   // clickTracker--;
   // console.log(productArray)
-  console.log('click tracker', clickTracker--);
-  if (clickTracker < 1){
+  // console.log("click tracker", clickTracker--);
+  if (clickTracker < 1) {
     saveLocalStorage();
-    // console.log(saveLocalStorage);
-    divEl.removeEventListener('click', handleClick);
-    
+    console.log(saveLocalStorage);
+    divEl.removeEventListener("click", handleClick);
+
     //TODO: This is showing up undefined
-    console.log('listed: ', productList.removeEventListener('click', handleClick));
+    console.log(
+      "listed: ",
+      productList.removeEventListener("click", handleClick)
+    );
   } else {
-    imageGenerator()
+    imageGenerator();
   }
+  renderChart();
   // console.log(clickTracker)
   if (clickTracker === 0) {
-        for (i = 0; i < productArray.length; i++) {
-          productArray[i].renderProductList();
+    for (i = 0; i < productArray.length; i++) {
+      productArray[i].renderProductList();
+    }
   }
-}
-// stopClicking();
-// saveLocalStorage();
-//   if (clickTracker === 0) {
-//     for (i = 0; i < productArray.length; i++) {
-//       productArray[i].renderProductList();
-//     } else {
+  // renderchart();
+
+  // stopClicking();
+  // saveLocalStorage();
+  //   if (clickTracker === 0) {
+  //     for (i = 0; i < productArray.length; i++) {
+  //       productArray[i].renderProductList();
+  //     } else {
   // imageGenerator();
 
-//     // renderChart();
-//   }
-// }
+  //     // renderChart();
+  //   }
+  // }
 }
 divEl.addEventListener("click", handleClick);
 loadLocalStorage();
 fillArray();
 imageGenerator();
-// saveLocalStorage();
 
+// saveLocalStorage();
